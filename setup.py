0fa7cc767fb5bd704c63658058be93bf4eb2ddce
Stéphanie Ricardo <227995391+tiffsteph-proj@users.noreply.github.com>
2025-09-08 22:04:24 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+# setup.py
+from setuptools import setup
+from setuptools import find_packages
+
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+
+setup(name='bankloan',
+      description="Bank Loan Prediction",
+      packages=find_packages(), # NEW: find packages automatically
+      install_requires=requirements)
