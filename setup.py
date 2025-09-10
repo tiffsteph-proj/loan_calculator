8d6e967054ba1e78bcbd85d4cd08a8606cbfd43f
Stéphanie Ricardo <227995391+tiffsteph-proj@users.noreply.github.com>
2025-09-10 08:32:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 
-# setup.py
-from setuptools import setup
-from setuptools import find_packages
-
 # list dependencies from file
 with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content]
 
-
-setup(name='bankloan',
-      description="Loan Calculator",
-      packages=find_packages(), # NEW: find packages automatically
-      install_requires=requirements)
